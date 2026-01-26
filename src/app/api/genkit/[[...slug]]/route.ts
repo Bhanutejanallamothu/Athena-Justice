b76d17e76cae245d383337f77cfade9e8bbe96b7
Siva Sudhamsh <sivasudhamsh2005@gmail.com>
2026-01-26 18:33:40 +0000

    ./node_modules/@opentelemetry/instrumentation/build/esm/platform/index.j

--- src/app/api/genkit/[[...slug]]/route.ts
+++ src/app/api/genkit/[[...slug]]/route.ts
@@ -1,4 +1,4 @@
-import {createNextJsHandler} from '@genkit-ai/next';
+import { NextJsHandler } from '@genkit-ai/next';
 
 // This is where all the Genkit flows are actually defined.
 import '@/ai/dev';
@@ -6,4 +6,4 @@ import '@/ai/dev';
 export const maxDuration = 60; // This function can run for a maximum of 60 seconds
 
 // By default, this will be served at `/api/genkit`.
-export const {GET, POST} = createNextJsHandler();
+export const { GET, POST } = NextJsHandler();
